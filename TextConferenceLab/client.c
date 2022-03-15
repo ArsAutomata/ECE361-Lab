a5bea7212bbd4820ef75f477b77ec07c129eda71
Dylan Sun <dylansun@Dylans-Mac-mini.local>
2022-03-14 20:13:45 -0400

    2 more functions added

--- TextConferenceLab/client.c
+++ TextConferenceLab/client.c
@@ -10,22 +10,37 @@
 #include <stdbool.h>
 #include "message.h"
 #define COMMAND_LEN 100
-
+#define BUFFFER_SIZE 1200
 bool logged_in;
 bool in_session;
 
-//socket 
+// socket
 int sockfd;
 int num_bytes;
 socklen_t servaddr_len;
 struct sockaddr_in servaddr;
 
-//buffer
-char[MAX_NAME + MAX_DATA + 20];
+// buffer
+char buffer[BUFFFER_SIZE];
+
+bool send_buffer()
+{
+	int num_bytes;
+
+	if ((num_bytes = send(sockfd, buffer, BUFFFER_SIZE - 1, 0)) == -1)
+	{
+		return true;
+	}
+	else
+	{
+		printf("Could not send.\n");
+		return false;
+	}
+}
 
 void login(char *client_id, char *password, char *server_ip, char *server_port)
 {
-	//create socket and conenct to server
+	// create socket and conenct to server
 	char *server_address = server_ip;
 	int port = atoi(server_port);
 
@@ -43,7 +58,7 @@ void login(char *client_id, char *password, char *server_ip, char *server_port)
 	servaddr.sin_port = htons(port);
 	servaddr.sin_addr.s_addr = inet_addr(server_address);
 
-	//prepare the message
+	// prepare the message
 	Message login_mes;
 	login_mes.type = JOIN;
 	strcpy(login_mes.source, client_id);
@@ -55,8 +70,8 @@ void login(char *client_id, char *password, char *server_ip, char *server_port)
 	int mes_len = strlen(mes_string);
 
 	logged_in = true;
-	//send the message
-	num_bytes = sendto(sockfd, (const char *) mes_string, mes_len, MSG_CONFIRM, (const struct sockaddr *) &servaddr, sizeof(servaddr));
+	// send the message
+	//num_bytes = sendto(sockfd, (const char *)mes_string, mes_len, mes_string, (const struct sockaddr *)&servaddr, sizeof(servaddr));
 
 	// Check if something was received
 	if (num_bytes == -1)
@@ -86,7 +101,7 @@ void logout()
 	int mes_len = strlen(mes_string);
 
 	int num_bytes;
-	num_bytes = sendto(sockfd, (const char *) mes_string, mes_len, MSG_CONFIRM, (const struct sockaddr *) &servaddr, sizeof(servaddr));
+	//num_bytes = sendto(sockfd, (const char *)mes_string, mes_len, MSG_CONFIRM, (const struct sockaddr *)&servaddr, sizeof(servaddr));
 	strcpy(mes_string, buffer);
 
 	// Check if something was received
@@ -105,33 +120,29 @@ void logout()
 void createsession(char *session_id)
 {
 
+	printf("creating session: %s \n", session_id);
+
 	Message create_mes;
 	create_mes.type = NEW_SESS;
 	strncpy(create_mes.data, session_id, 200);
 	create_mes.size = strlen(create_mes.data);
 
 	char *create_string = serialize(create_mes);
-	int num_bytes;
 	strcpy(create_string, buffer);
 
-	if ((num_bytes = send(*sockfd, buffer, BUF_SIZE - 1, 0)) == -1)
-	{
-		return;
-	}
-	else
-	{
-		printf("Could not send.\n");
-		return;
-	}
+	send_buffer();
+	return;
 }
 
 void joinsession(char *session_id)
 {
 	if (in_session)
 	{
-		fprintf("You are in a session.\n");
+		printf("You are in a session.\n");
 		return;
 	}
+
+	printf("joining: %s\n", session_id);
 
 	Message join_mes;
 	join_mes.type = JOIN;
@@ -142,51 +153,50 @@ void joinsession(char *session_id)
 	int num_bytes;
 	strcpy(join_string, buffer);
 
-	if ((num_bytes = send(*sockfd, buffer, BUF_SIZE - 1, 0)) == -1)
+	if (send_buffer() == true)
 	{
+		in_session = true;
 		return;
 	}
-	else
-	{
-		printf("Could not send.\n");
-		return;
-	}
+	return;
 }
 
 void leavesession()
 {
 	if (!in_session)
 	{
-		fprintf("You are not in a session.\n");
+		printf("You are not in a session.\n");
 		return;
 	}
+
+	printf("leaving session\n");
 
 	Message leave_mes;
 	leave_mes.type = LEAVE_SESS;
 	leave_mes.size = 0;
 
-	char *leave_string = serialize(join_mes);
+	char *leave_string = serialize(leave_mes);
 	int num_bytes;
 	strcpy(leave_string, buffer);
 
-	if ((num_bytes = send(*sockfd, buffer, BUF_SIZE - 1, 0)) == -1)
+	if (send_buffer() == true)
 	{
+		in_session = false;
 		return;
 	}
-	else
-	{
-		printf("Could not send.\n");
-		return;
-	}
+
+	return;
 }
 
 void list()
 {
 	if (!in_session)
 	{
-		fprintf("You are not in a session.\n");
+		printf("You are not in a session.\n");
 		return;
 	}
+
+	printf("printing list:\n");
 
 	Message list_mes;
 	list_mes.type = QUERY;
@@ -196,20 +206,29 @@ void list()
 	int num_bytes;
 	strcpy(list_string, buffer);
 
-	if ((num_bytes = send(*sockfd, buffer, BUF_SIZE - 1, 0)) == -1)
-	{
-		return;
-	}
-	else
-	{
-		printf("Could not send.\n");
-		return;
-	}
+	send_buffer();
+	return;
+}
+
+void send_text(char* text)
+{
+	int numbytes;
+	Message text_mes;
+	text_mes.type = MESSAGE;
+
+	strcpy(text_mes.data, text);
+	text_mes.size = strlen(text_mes.data);
+	char *text_string = serialize(text_mes);
+
+	strcpy(text_string, buffer);
+
+	send_buffer();
+	return;
 }
 
 int main()
 {
-	//input strings
+	// input strings
 	char cmd[COMMAND_LEN];
 	char client_id[COMMAND_LEN];
 	char session_id[COMMAND_LEN];
@@ -217,67 +236,24 @@ int main()
 	char server_ip[COMMAND_LEN];
 	char server_port[COMMAND_LEN];
 
-	//socket 
+	// socket
 	int sockfd;
 	int num_bytes;
 	socklen_t servaddr_len;
 	struct sockaddr_in servaddr;
 
-	//status 
+	// status
 	bool logged_in = false;
 
-	//login and make connection
-	PRELOG:
-		while (!logged_in)
-		{
-			scanf("%s", cmd);
-			if (strcmp(cmd, "/login") == 0)
-			{
-				scanf(" %s %s %s %s", client_id, password, server_ip, server_port);
-
-				login(client_id, password, server_ip, server_port);
-			}
-			else if (strcmp(cmd, "/quit") == 0)
-			{
-				return 0;
-			}
-			else
-			{
-				printf("please login first\n");
-			}
-		}
-
-	//full access to menu
-	while (logged_in)
+	// login and make connection
+	while (!logged_in)
 	{
 		scanf("%s", cmd);
+		if (strcmp(cmd, "/login") == 0)
+		{
+			scanf(" %s %s %s %s", client_id, password, server_ip, server_port);
 
-		//logout command
-		if (strcmp(cmd, "/logout") == 0)
-		{
-			logout();
-		}
-
-		//join session command
-		else if (strcmp(cmd, "/joinsession") == 0)
-		{
-			scanf(" %s", session_id);
-			joinsession(session_id);
-		}
-		else if (strcmp(cmd, "/leavesession") == 0)
-		{
-			leavesession();
-			printf("left session");
-		}
-		else if (strcmp(cmd, "/createsession") == 0)
-		{
-			scanf(" %s", session_id);
-			createsession(session_id);
-		}
-		else if (strcmp(cmd, "/list") == 0)
-		{
-			printf("list");
-			list();
+			login(client_id, password, server_ip, server_port);
 		}
 		else if (strcmp(cmd, "/quit") == 0)
 		{
@@ -285,7 +261,98 @@ int main()
 		}
 		else
 		{
-			printf("invalid command\n");
+			printf("please login first\n");
+		}
+	}
+
+	// full access to menu
+	while (logged_in)
+	{
+		while (!in_session)
+		{
+			scanf("%s", cmd);
+
+			if (strcmp(cmd, "/login") == 0)
+			{
+				printf("already logged in");
+			}
+			else if (strcmp(cmd, "/logout") == 0)
+			{
+				logout();
+			}
+			else if (strcmp(cmd, "/joinsession") == 0)
+			{
+				scanf(" %s", session_id);
+				joinsession(session_id);
+			}
+			else if (strcmp(cmd, "/leavesession") == 0)
+			{
+				printf("invalid at this time\n");
+			}
+			else if (strcmp(cmd, "/createsession") == 0)
+			{
+				scanf(" %s", session_id);
+				createsession(session_id);
+			}
+			else if (strcmp(cmd, "/list") == 0)
+			{
+				list();
+			}
+			else if (strcmp(cmd, "/quit") == 0)
+			{
+				return 0;
+			}
+			else
+			{
+				printf("invalid command while not in a session\n");
+			}
+		}
+
+		while (in_session)
+		{
+			scanf("%s", cmd);
+
+			if (strcmp(cmd, "/login") == 0)
+			{
+				printf("already logged in\n");
+			}
+			else if (strcmp(cmd, "/logout") == 0)
+			{
+				logout();
+			}
+			else if (strcmp(cmd, "/joinsession") == 0)
+			{
+				printf("already in a session\n");
+			}
+			else if (strcmp(cmd, "/leavesession") == 0)
+			{
+				leavesession();
+			}
+			else if (strcmp(cmd, "/createsession") == 0)
+			{
+				scanf(" %s", session_id);
+				createsession(session_id);
+			}
+			else if (strcmp(cmd, "/list") == 0)
+			{
+				printf("list");
+				list();
+			}
+			else if (strcmp(cmd, "/quit") == 0)
+			{
+				return 0;
+			}
+			else
+			{
+				char total_text[BUFFFER_SIZE];
+				char partial_text[800];
+				scanf("%[^\n]s", partial_text);
+
+				strcat(total_text, cmd);
+				strcat(total_text, partial_text);
+				
+				send_text(total_text);
+			}
 		}
 	}
 
--- TextConferenceLab/message.h
+++ TextConferenceLab/message.h
@@ -14,23 +14,20 @@
 #define MAX_NAME 100
 #define MAX_DATA 1000
 
-
 char ID_arr[5][MAX_NAME] = {
-    "pete",
-    "julia",
-    "luigi",
-    "mr.cow",
-    "piss",
-	"123"
-};
+	"pete",
+	"julia",
+	"luigi",
+	"mr.cow",
+	"piss",
+	"123"};
 char pw_arr[5][20] = {
-    "1234ttyu",
-    "3456gv",
-    "password3",
-    "mr.password",
-    "peeword",
-	"123"
-};
+	"1234ttyu",
+	"3456gv",
+	"password3",
+	"mr.password",
+	"peeword",
+	"123"};
 
 typedef enum TYPES
 {
@@ -50,51 +47,50 @@ typedef enum TYPES
 	QU_ACK
 };
 
-typedef struct Message
+typedef struct message
 {
 	unsigned int type;
 	unsigned int size;
 	unsigned char source[MAX_NAME];
 	unsigned char data[MAX_DATA];
-};
+} Message;
 
-//reference 1 https://stackoverflow.com/questions/9210528/split-string-with-delimiters-in-c
-//reference 2 https://stackoverflow.com/questions/3889992/how-does-strtok-split-the-string-into-tokens-in-c
-Message* deserialize(char *string)
+// reference 1 https://stackoverflow.com/questions/9210528/split-string-with-delimiters-in-c
+// reference 2 https://stackoverflow.com/questions/3889992/how-does-strtok-split-the-string-into-tokens-in-c
+Message *deserialize(char *string)
 {
-	Message *new_message = malloc(sizeof(struct Message));
+	Message *new_message = malloc(sizeof(Message));
 
 	char *typevar;
 	char *size;
 	char *source;
 	char *data;
 
-	//store
+	// store
 	typevar = strtok(string, ":");
 	size = strtok(NULL, ":");
 	source = strtok(NULL, ":");
 	data = strtok(NULL, "");
 
-	//exception handle
+	// exception handle
 	if (typevar == NULL || size == NULL || source == NULL || data == NULL)
 	{
 		return NULL;
 	}
 
-	//store
+	// store
 	new_message->type = atoi(typevar);
 	new_message->size = atoi(size);
-	strcpy((char*) new_message->source, source);
-	strcpy((char*) new_message->data, data);
+	strcpy((char *)new_message->source, source);
+	strcpy((char *)new_message->data, data);
 
 	return new_message;
 }
 
-
 // reference 3 https://stackoverflow.com/questions/48422573/writing-to-file-if-sprintf-buffer-is-overflow
 char *serialize(Message msg)
 {
-	char *string = malloc((MAX_NAME+MAX_DATA+20) *sizeof(char));
+	char *string = malloc((MAX_NAME + MAX_DATA + 20) * sizeof(char));
 
 	sprintf(string, "%d:%d:%s:%s", msg.type, msg.size, msg.source, msg.data);
 
